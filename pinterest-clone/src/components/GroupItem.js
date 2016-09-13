a3be574e065543fd3e693707f81b8ac495d15b59
Shayne Meyer <shaynemeyer@Shaynes-MacBook-Pro.local>
2016-09-13 06:40:14 -0700

    trying to get stupid images to work

--- pinterest-clone/src/components/GroupItem.js
+++ pinterest-clone/src/components/GroupItem.js
@@ -1,12 +1,14 @@
 import React from 'react';
-import diyPic from '../images/pinterest-diy.png';
 
 let GroupItem = React.createClass({
 	render: function () {
+		//let imgSrc = `images/${this.props.imgSrc}`;
 		return (
 			<div className="well well-sm group-item col-sm-4">
-
-				<img src={diyPic} alt="DIY" />
+				<div className="item-title">
+					{this.props.itemText}
+				</div>
+				<img src={this.props.imgSrc} alt={this.props.itemText} />
 
 			</div>
 		);
--- pinterest-clone/src/components/GroupList.js
+++ pinterest-clone/src/components/GroupList.js
@@ -5,12 +5,18 @@ let GroupList = React.createClass({
 	render: function () {
 		return (
 			<div className="group-list-container">
-				<GroupItem />
-				<GroupItem />
-				<GroupItem />
-				<GroupItem />
-				<GroupItem />
-				<GroupItem />
+				<GroupItem itemText="DIY - Tips Tricks Ideas"
+				           imgSrc="pinterest-diy.png"/>
+				<GroupItem itemText="AVIATION"
+				           imgSrc="pinterest-aviation.png"/>
+				<GroupItem itemText="Exception Aviation"
+				           imgSrc="pinterest-exceptional.png"/>
+				<GroupItem itemText="AEROSPACE"
+				           imgSrc="pinterest-aerospace.png"/>
+				<GroupItem itemText="WHEN FLYING WAS WORTH IT"
+				           imgSrc="pinterest-when.png"/>
+				<GroupItem itemText="Aircraft Recycled into Furniture"
+				           imgSrc="pinterest-recycled.png"/>
 			</div>
 		);
 	}

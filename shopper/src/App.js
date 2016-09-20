5d5c2f3c00f6366cdc9f8ed7cd3b86921695f25e
Shayne Meyer <shayne.meyer@gmail.com>
2016-09-20 06:44:14 -0700

    added cartpage

--- shopper/src/App.js
+++ shopper/src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import './App.css';
 import Nav from './Nav';
 import ItemPage from './ItemPage';
+import CartPage from './CartPage';
 import {items} from './data/static-data';
 
 class App extends Component {
@@ -38,8 +39,34 @@ class App extends Component {
             onAddToCart={this.handleAddToCart}/>
         );
       case 1:
-        return <span>Cart</span>;
+        return this.renderCart();
     }
+  }
+
+  renderCart() {
+    // count how many of each item is in the cart
+    let itemCounts = this.state.cart.reduce((itemCounts, itemId) => {
+      itemCounts[itemId] = itemCounts[itemId] || 0;
+      itemCounts[itemId]++;
+      return itemCounts;
+    }, {});
+
+    // create an array of items
+    let keys = Object.keys(itemCounts);
+    let cartItems = keys.map(itemId => {
+      // find the item by its id
+      var item = items.find(item => item.id === parseInt(itemId, 10));
+
+      // create a new 'item' that also has a 'count' property
+      return {
+        ...item,
+        count: itemCounts[itemId]
+      }
+    });
+
+    return (
+      <CartPage items={cartItems} />
+    );
   }
 
   render() {

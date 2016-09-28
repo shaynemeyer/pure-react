11dafc644e225c4fa75e5f4744296ad34ca04b75
Shayne Meyer <shayne.meyer@gmail.com>
2016-09-27 21:06:39 -0700

    added people data

---
+++ slack-react/src/data/people.js
@@ -0,0 +1,28 @@
+let people = [
+	{
+		id: 1,
+		name: "Dave"
+	},
+	{
+		id: 2,
+		name: "Sarah"
+	},
+	{
+		id: 3,
+		name: "Zack"
+	},
+	{
+		id: 4,
+		name: "Pam"
+	},
+	{
+		id: 5,
+		name: "Erin"
+	},
+	{
+		id: 6,
+		name: "Joe"
+	}
+];
+
+export {people};
